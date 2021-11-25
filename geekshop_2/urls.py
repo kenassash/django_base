735b63927f4f7508415ba77e36557bc0d760f18b
kenassash <68361165+kenassash@users.noreply.github.com>
2021-11-26 05:49:08 +0900

    Add files via upload
    all

---
+++ geekshop_2/urls.py
@@ -0,0 +1,37 @@
+"""geekshop_2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from mainapp import views as mainapp
+
+urlpatterns = [
+    path('', mainapp.index, name='index'),
+    path('contacts/', mainapp.contact, name='contact'),
+
+    path('products/', include('mainapp.urls', namespace='products')),
+    path('products/home/', mainapp.products_home, name='products_home'),
+    path('products/modern/', mainapp.products_modern, name='products_modern'),
+    path('products/office/', mainapp.products_office, name='products_office'),
+    path('products/classic/', mainapp.products_classic, name='products_classic'),
+
+
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
