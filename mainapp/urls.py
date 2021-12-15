943ad6998b8e0809439cedb8b6e5fa010448c4b3
sash <kenas.sash@gmail.com>
2021-12-15 21:58:00 +0900

    Lesson_8

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,6 +7,8 @@ app_name = 'products'
 urlpatterns = [
     path('', mainapp.products, name='products'),
     path('<int:pk>/', mainapp.products, name='products_category'),
+    path('<int:pk>/<int:page>/', mainapp.products, name='product_paginate'),
     path('product/<int:pk>/', mainapp.product, name='product')
+
 
 ]
