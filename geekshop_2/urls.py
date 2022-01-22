4051026ba62557b4fe81b26d433e097d7e643b5f
sash <kenas.sash@gmail.com>
2022-01-22 10:48:40 +0900

    Измененный settings.py и urls

--- geekshop_2/urls.py
+++ geekshop_2/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from mainapp import views as mainapp
 
 urlpatterns = [
@@ -30,8 +30,13 @@ urlpatterns = [
     path('orders/', include('ordersapp.urls', namespace='ordersapp')),
     path('admin/', include('adminapp.urls', namespace='adminapp')),
 
-    #path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
